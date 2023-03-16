a6179b9c5b59be9c49f936c41cbb240ac0ae7028
anhongkui <anhongkui@jd.com>
2023-03-16 18:18:15 +0800

    image ok
    mouse ok but with lag

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/GoFeGroup/gordp/glog"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//glog.SetLevel(glog.NONE)
+	glog.SetLevel(glog.NONE)
 
 	r := gin.Default()
 	r.GET("/api/rdp", rdpProxy)
